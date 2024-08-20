4e98e681c975bdcba3e15554eb3f3471ff40994e
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2024-08-20 23:34:56 +0530

    update font family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,17 @@
 import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 
 export default {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
